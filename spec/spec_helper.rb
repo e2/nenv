a9072a06c72b906413beb2f91d264dc02d58081b
Cezary Baginski <cezary@chronomantic.net>
2014-12-13 21:45:16 +0100

    unstub some ENV values for Pry to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,11 @@ RSpec.configure do |config|
     allow(ENV).to receive(:[]=) do |key, value|
       fail "stub me: ENV[#{key.inspect}] = #{value.inspect}"
     end
+
+    allow(ENV).to receive(:[]).with('PRYRC').and_call_original
+    allow(ENV).to receive(:[]).with('DISABLE_PRY').and_call_original
+    allow(ENV).to receive(:[]).with('ANSICON').and_call_original
+    allow(ENV).to receive(:[]).with('TERM').and_call_original
   end
 
   config.after do
